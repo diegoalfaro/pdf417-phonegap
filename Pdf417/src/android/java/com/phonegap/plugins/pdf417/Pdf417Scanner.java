97ace104f8b4e7506caa212dbf779e51470f8ffc
Diego Hernán Alfaro <30939690+diegoalfaro@users.noreply.github.com>
2020-10-02 15:49:41 -0300

    Update Pdf417Scanner.java

--- Pdf417/src/android/java/com/phonegap/plugins/pdf417/Pdf417Scanner.java
+++ Pdf417/src/android/java/com/phonegap/plugins/pdf417/Pdf417Scanner.java
@@ -10,7 +10,7 @@ package com.phonegap.plugins.pdf417;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 
 import com.microblink.MicroblinkSDK;
